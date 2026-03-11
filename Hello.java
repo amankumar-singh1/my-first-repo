76ba6ea4a3b1c234bd10564a9f0e3dd81e3f6b2e
amankumar-singh1 <amansingh01069@gmail.com>
2026-03-12 00:37:58 +0530

    Update str2 to a longer string

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
    public static void main(String[] args) {
        String str1 = "hello";
-       String str2 = "aman";
+       String str2 = "amannnn Sir";
        String  con = str1 +" "+ str2;
        System.out.println("Concatenation of " + str1 + " and " + str2 + " is: " + con);
    }

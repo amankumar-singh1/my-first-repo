71d75d924817143aa03b82663709d76131d02655
amankumar-singh1 <amansingh01069@gmail.com>
2026-03-12 00:26:09 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello{
-   public static void main(String[] args) {
-       String str1 = "hello";
-       String str2 = "aman";
-       String  con = str1 +" "+ str2;
-       System.out.println("Contactination of " + str1 + " and " + str2 + " is: " + con);
-   }
-}
